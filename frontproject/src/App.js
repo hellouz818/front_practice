02b1af480b790f93533cccfef41ba7d13abd95ae
hyeju1123 <hjjun1123@gmail.com>
2021-10-20 19:14:12 +0900

    edit mainpage

--- frontproject/src/App.js
+++ frontproject/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Plant from './images/plantIcon.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="mainContainer">
+        <div className="mainContentsWrapper">
+          <div className="mainHeader">
+            <div className='logo'>LO</div>
+            <div className="headerNavigator">
+              <div className="headerButton">ABOUT</div>
+              <div className="headerButton">ARTICLE</div>
+              <div className="headerButtonLine">SUBSCRIBE</div>
+            </div>
+          </div>
+          <div className="mainTitleWrapper">
+            <p className="mainTitle">LOMBOK</p>
+            <p className="subTitle">HOLISTIC HEALTH</p>
+            <hr className="smallLine" />
+          </div>
+          <div className="imageWrapper">
+            <img src={Plant} className="plantIcon1" alt="logo" />
+            <img src={Plant} className="plantIcon2" alt="logo" />
+          </div>
+        </div>
+          
+      </div>
+      <div className="mainContainer"></div>
+    </>
   );
 }
 
