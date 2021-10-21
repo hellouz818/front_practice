86b02b1c7da0fbf24b492ab67642eb4e50db4f65
hyeju1123 <hjjun1123@gmail.com>
2021-10-21 20:25:45 +0900

    file upload

--- frontproject/src/main/FourthPage.js
+++ frontproject/src/main/FourthPage.js
@@ -1,15 +1,30 @@
+import { useState } from 'react'
 import './FourthPage.css'
 import axios from 'axios'
 
 
 function FourthPage() {
 
+    const [file, setFile] = useState('')
+
     const testApi = () => {
+        let formData = new FormData();
         let title = 'first test';
-        let contents = 'first contents';
-        axios.post('http://172.30.1.37:8000/testApi', {
-            title, contents
+        let content = 'first contents';
+        formData.append('file', file)
+        formData.append('title', title)
+        formData.append('content', content)
+        
+        axios.post('http://127.0.0.1:8000/testApi/', {
+            formData
+        }).then(data => {
+            console.log(data)
         })
+    }
+
+    const onChange = (e) => {
+        setFile(e.target.files[0])
+        console.log(e.target.files[0])
     }
 
     return (
@@ -19,7 +34,7 @@ function FourthPage() {
                 <div className="subscribeSubText">Sign-up to our newsletter</div>
             </div>
             <div className="subscribeBox">
-                <input className="subscribeInputBox" placeholder="Your email" />
+                <input type="file" onChange={onChange} className="subscribeInputBox" placeholder="Your email" />
                 <div className="subscribeSubmitButton" onClick={testApi}>SUBMIT</div>
             </div>
         </div>
