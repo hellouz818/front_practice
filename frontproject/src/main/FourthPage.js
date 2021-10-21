0c8f776cb9a1a0343da119e0b35556c5a5316401
hyeju1123 <hjjun1123@gmail.com>
2021-10-21 19:11:50 +0900

    test api

--- frontproject/src/main/FourthPage.js
+++ frontproject/src/main/FourthPage.js
@@ -1,6 +1,17 @@
 import './FourthPage.css'
+import axios from 'axios'
+
 
 function FourthPage() {
+
+    const testApi = () => {
+        let title = 'first test';
+        let contents = 'first contents';
+        axios.post('http://172.30.1.37:8000/testApi', {
+            title, contents
+        })
+    }
+
     return (
         <div className="mainContainer4">
             <div className="subscribeHeader">
@@ -9,7 +20,7 @@ function FourthPage() {
             </div>
             <div className="subscribeBox">
                 <input className="subscribeInputBox" placeholder="Your email" />
-                <div className="subscribeSubmitButton">SUBMIT</div>
+                <div className="subscribeSubmitButton" onClick={testApi}>SUBMIT</div>
             </div>
         </div>
     )
